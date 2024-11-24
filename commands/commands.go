116920c6e08d085e2b88495d2005335cc3fa5f1b
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2024-11-24 19:37:32 +0100

    Implement SET and GET commands

--- commands/commands.go
+++ commands/commands.go
@@ -4,10 +4,25 @@ import "github.com/ozedd-ee/redis-go/serializer"
 
 var s = serializer.Serializer{}
 
+var store map[string]string
+
 func ping() string {
 	return s.SerializeSimpleString("PONG")
 }
 
 func echo(msg string) string {
 	return s.SerializeSimpleString(msg)
+}
+
+func set(key string, val string) string {
+	store[key] = val
+	return s.SerializeSimpleString("OK")
+}
+
+func get(key string) string {
+	val, ok := store[key] 
+	if !ok {
+		return s.Null()
+	}
+	return s.SerializeBulkString(val)
 }
--- commands/handler.go
+++ commands/handler.go
@@ -6,9 +6,9 @@ import (
 	"github.com/ozedd-ee/redis-go/serializer"
 )
 
-func HandleCommand(c string, *serializer.Serializer) string {
-	// split command string into array of the command and its options
-	cmdArr := strings.Split(c, " ")
+func HandleCommand(c string, s *serializer.Serializer) string {
+	// split command string into array of the command and its options using CRLF separator retained during deserialization
+	cmdArr := strings.Split(c, "\r\n")
 	cmd := cmdArr[0]
 
 	switch cmd {
@@ -18,8 +18,19 @@ func HandleCommand(c string, *serializer.Serializer) string {
 		if len(cmdArr) < 2 {
 			return s.SerializeSimpleError("err", "No message to ECHO")
 		}
-		msg := strings.Join(cmdArr[1:], " ")
+		msg := cmdArr[1]
 		return echo(msg)
+	case "SET":
+		if len(cmdArr) < 3 {
+			return s.SerializeSimpleError("err", "No value specified for key")
+		}
+		key, val := cmdArr[1], cmdArr[2]
+		return set(key, val)
+	case "GET":
+		if len(cmdArr) < 2 {
+			return s.SerializeSimpleError("err", "No key specified")
+		}
+		return get(cmdArr[1])
 	default:
 		return s.SerializeSimpleError("err", "Invalid command")
 	}
