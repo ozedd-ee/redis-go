1f6380938c7e89fd41e02d04379df6dbfaa36eb5
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2024-11-28 21:12:08 +0100

    clean-up: Remove unnecessary length checks

--- serializer/deserializer.go
+++ serializer/deserializer.go
@@ -7,38 +7,34 @@ import (
 )
 
 func (s *Serializer) DeserializeMessage(respArr string) string {
-    if string(respArr[0]) != "*" {
+	if string(respArr[0]) != "*" {
 		log.Fatal("Message is not a RESP array")
 	}
 	respArr = respArr[1:]
 	// Split at first CRLF to separate array length from elements
 	arrLengthString, respArr, found := strings.Cut(respArr, "\r\n")
-    // for storing plain command strings
-    var plainCmdArr []string;
+	// for storing plain command strings
+	var plainCmdArr []string
 	if found {
 		// Try converting length to integer
-		arrLength, err := strconv.Atoi(string(arrLengthString))
+		_, err := strconv.Atoi(string(arrLengthString))
 		if err != nil {
 			log.Fatal("Array length not specified")
 		}
 
 		// Split RESP array of bulk strings into Go array of RESP bulk strings (without the identifiers)
-		bulkStringArr := strings.Split(respArr, "$")
-
-        // Check to ensure that the specified length matches the actual length
-        if (len(bulkStringArr) != arrLength) {
-            log.Fatal("Specified length does not match actual length")
-        }
+		_, bulkSA, _ := strings.Cut(respArr, "$")
+		bulkStringArr := strings.Split(bulkSA, "$")
 
 		for _, v := range bulkStringArr {
-            // Remove length and following CRLF
-            _, noPrefixV, success := strings.Cut(v, "\r\n")
+			// Remove length and following CRLF
+			_, noPrefixV, success := strings.Cut(v, "\r\n")
 			if !success {
-                log.Fatal("No opening CRLF for bulk string")
-            }
-            // append cleaned string to array (Retain closing CRLF as separator)
-            plainCmdArr = append(plainCmdArr, noPrefixV)
+				log.Fatal("No opening CRLF for bulk string")
+			}
+			// append cleaned string to array (Retain closing CRLF as separator)
+			plainCmdArr = append(plainCmdArr, noPrefixV)
 		}
 	}
-    return strings.Join(plainCmdArr, "")
+	return strings.Join(plainCmdArr, "")
 }
